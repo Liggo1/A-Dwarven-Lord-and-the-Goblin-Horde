51abe18ebd01af918cd6a656ec4e33ac92edc1c9
Liggo1 <iggli547@student.otago.ac.nz>
2024-12-10 21:58:02 +1300

    Test Commit

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -11,7 +11,7 @@ public class CameraControl : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //test push change here
     }
 
     // Update is called once per frame
